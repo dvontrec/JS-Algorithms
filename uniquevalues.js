22bc796de6920b66c8cd87a06a74d21257519fb5
Dvontre Coleman <dvontrec@gmail.com>
2018-12-11 15:11:21 -0500

    Add count unique values

--- uniquevalues.js
+++ uniquevalues.js
@@ -23,6 +23,6 @@ function countUniqueValues(arr) {
   return uTotal;
 }
 
-console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 1, 1, 2]));
-console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13]));
-console.log(countUniqueValues([]));
+console.log(countUniqueValues([1, 1, 1, 1, 1, 1, 1, 1, 2])); // 2
+console.log(countUniqueValues([1, 2, 3, 4, 4, 4, 7, 7, 12, 12, 13])); // 7
+console.log(countUniqueValues([])); // 0
