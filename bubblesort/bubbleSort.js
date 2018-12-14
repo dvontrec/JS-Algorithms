db63fafb65227e0ccd4375593a17687938ad484f
Dvontre Coleman <dvontrec@gmail.com>
2018-12-14 09:55:59 -0500

    Add optimized bubble sort

--- bubblesort/bubbleSort.js
+++ bubblesort/bubbleSort.js
@@ -16,5 +16,36 @@ function bubbleSort(arr) {
   // return sorted array
   return arr;
 }
+function optimizedBubbleSort(arr) {
+  // DO stuff
+  // create a variable to see if we swapped last loop
+  let swapped = true;
+  // loops through from end of array to the beginning
+  for (let i = arr.length - 1; i >= 0; i--) {
+    // swap is false until we swap
+    swapped = false;
+    // loop through array from the beginning to i
+    for (let j = 0; j < i; j++) {
+      //if arr[j] is greater than j[i] swap them
+      if (arr[j] > arr[i]) {
+        let temp = arr[i];
+        arr[i] = arr[j];
+        arr[j] = temp;
+        swapped = true;
+      }
+    }
+    // if no swaps break
+    if (!swapped) {
+      break;
+    }
+  }
+  // return sorted array
+  return arr;
+}
 
 console.log(bubbleSort([3, 1, 4, 12, -1, -8, 103, 6, 33, 5, 11, 2, 58, 0]));
+console.log(
+  optimizedBubbleSort([3, 1, 4, 12, -1, -8, 103, 6, 33, 5, 11, 2, 58, 0])
+);
+console.log(bubbleSort([1, 2, 3, 5, 4, 6]));
+console.log(optimizedBubbleSort([1, 2, 3, 5, 4, 6]));
