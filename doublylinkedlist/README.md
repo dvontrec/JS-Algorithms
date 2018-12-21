0035110735b4a4dd9377098a54ed9ed7ba5f2878
Dvontre Coleman <dvontrec@gmail.com>
2018-12-21 10:11:18 -0500

    Add doubly linked list basics

---
+++ doublylinkedlist/README.md
@@ -0,0 +1,8 @@
+# Doubly Linked List
+
+## Node
+
+- stores 3 things
+  - Piece of data - val
+  - Reference to the previous node
+  - Reference to next node
---
+++ doublylinkedlist/doublyLinkedList.js
@@ -0,0 +1,16 @@
+// Node class
+class Node {
+  constructor(val) {
+    this.val = val;
+    this.next = null;
+    this.prev = null;
+  }
+}
+// list class
+class DoublyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+}
