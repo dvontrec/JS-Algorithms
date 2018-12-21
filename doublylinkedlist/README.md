b1d12fb96aa3b6c80c1f9e44e3dff9cc8bf9cfda
Dvontre Coleman <dvontrec@gmail.com>
2018-12-21 10:34:20 -0500

    Add doubly linked list push method

--- doublylinkedlist/README.md
+++ doublylinkedlist/README.md
@@ -6,3 +6,13 @@
   - Piece of data - val
   - Reference to the previous node
   - Reference to next node
+
+## Push
+
+- Accepts a value
+- Adds a new node to the end of the linkedlist
+- If the list is empty add the new node as the head and the tail
+- If there are elements set the prev property to be the current tail
+- Set the next of the tail to be the new node
+- Increment the length
+- Return the list
--- doublylinkedlist/doublyLinkedList.js
+++ doublylinkedlist/doublyLinkedList.js
@@ -13,4 +13,26 @@ class DoublyLinkedList {
     this.tail = null;
     this.length = 0;
   }
+
+  push(val) {
+    // creates a new node
+    let node = new Node(val);
+    // if the list length is 0 add as head and tail
+    if (this.length === 0) {
+      this.head = node;
+      this.tail = node;
+    } else {
+      // if there are already nodes n th list, add this as the new tail
+      node.prev = this.tail;
+      this.tail.next = node;
+      this.tail = node;
+    }
+    // Increment length by 1
+    this.length++;
+    return this;
+  }
 }
+
+let dList = new DoublyLinkedList();
+console.log(dList.push(1));
+console.log(dList.push(2));
