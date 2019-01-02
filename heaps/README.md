6dd14a50e10d0b8a85e811faba1478a451b9ccca
Dvontre Coleman <dvontrec@gmail.com>
2019-01-02 12:19:23 -0500

    Add Max Binary Heap extract max method

--- heaps/README.md
+++ heaps/README.md
@@ -13,3 +13,16 @@
   - Loop as ling as the n value is greater than the parent value
   - Swap the value of n with the parent
   - Set the index to be the parent index and start over
+
+## Extract Max (remove root)
+
+- Swap the first value in the values property with the last one
+- Pop from the values , so you return the value at the end (the old root)
+- Bubble down the new root to the correct spot
+  - parent index starts ayt 0
+  - find the index of the left child (2\* index +1) \*\* make sure inbounds
+  - find the index of the right child (2\* index +2) \*\* make sure inbounds
+  - if the left or right child is greater than the element swap, if both are greater swap the larger child
+  - the child index is swapped to now be the parent index
+  - keep looping and swapping until neither child is larger than the element
+- Return the old root
--- heaps/heaps.js
+++ heaps/heaps.js
@@ -18,8 +18,40 @@ class MaxBinaryHeap {
     }
     return this.values;
   }
+
+  extractMax() {
+    let values = this.values;
+    //swap the first and last values
+    let max = values[0];
+    values[0] = values[values.length - 1];
+    values.pop();
+    let parentIndex = 0;
+    // Buble down
+    while (true) {
+      let right = parentIndex * 2 + 1;
+      let left = parentIndex * 2 + 2;
+      let max;
+      // if there is no right only compare to the left
+      if (right >= values.length) {
+        max = left;
+      } else {
+        max = values[left] >= values[right] ? left : right;
+      }
+      // if max is greater than parent
+      if (values[max] > values[parentIndex]) {
+        let temp = values[parentIndex];
+        values[parentIndex] = values[max];
+        values[max] = temp;
+        parentIndex = max;
+      } else break;
+    }
+    console.log(this.values);
+    //Return old root
+    return max;
+  }
 }
 
 let bh = new MaxBinaryHeap();
 bh.values = [41, 39, 33, 18, 27, 12];
 console.log(bh.insert(55)); // [ 55, 39, 41, 18, 27, 12, 33 ]
+console.log(bh.extractMax()); // 55
