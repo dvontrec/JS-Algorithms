138ffa421e3a5340f9c8703200e9662c7aea6321
Dvontre Coleman <dvontrec@gmail.com>
2019-01-04 11:40:45 -0500

    Add remove vertex functinality

--- graphs/README.md
+++ graphs/README.md
@@ -16,3 +16,10 @@
 - Accepts 2 vertices
 - Reassign the key of vertex 1 to be an array that does not contain vertex 2
 - Reassign the key of vertex 2 to be an array that does not contain vertex 1
+
+## Remove A Vertex
+
+- Accepts the name of the vertex
+- Loops as long as there are other verteces in the adjacency list for this vertex
+  - Remove the edges for the vertex and all its connected verteces
+- Remove the key in the adjacency list for that vertex
--- graphs/graphs.js
+++ graphs/graphs.js
@@ -21,6 +21,13 @@ class Graph {
     this.adjacencyList[v1] = this.adjacencyList[v1].filter(key => key != v2); // filters the list leaving only keys that are not v2
     this.adjacencyList[v2] = this.adjacencyList[v2].filter(key => key != v1); // filters the list leaving only keys that are not v1
   }
+
+  removeVertex(vertex) {
+    // for each connection remove the edge
+    this.adjacencyList[vertex].forEach(v => this.removeEdge(vertex, v));
+    // remove the key from the object
+    delete this.adjacencyList[vertex];
+  }
 }
 
 let graph = new Graph();
@@ -29,8 +36,9 @@ graph.addVertex('Dallas');
 graph.addVertex('Aspen');
 graph.addEdge('Tokyo', 'Dallas');
 graph.addEdge('Dallas', 'Aspen');
+graph.addEdge('Tokyo', 'Aspen');
 
 console.log(graph.adjacencyList);
 
-graph.removeEdge('Tokyo', 'Dallas');
+graph.removeVertex('Dallas');
 console.log(graph.adjacencyList);
